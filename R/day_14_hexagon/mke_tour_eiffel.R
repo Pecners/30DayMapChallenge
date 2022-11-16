33d0b6aa1963b3991de46997d322005baae5980d
Spencer Schien <spencer.schien@gmail.com>
2022-11-15 20:14:25 -0600

    updated day 14 v2 caption

--- R/day_14_hexagon/mke_tour_eiffel.R
+++ R/day_14_hexagon/mke_tour_eiffel.R
@@ -5,50 +5,55 @@ library(elevatr)
 library(PrettyCols)
 library(ggfx)
 
+# set up Milwaukee boundaries
 wi <- tigris::places(state = "Wisconsin")
 
 mke <- wi |> 
   filter(NAME == "Milwaukee") |> 
   st_transform(8159)
 
-
+# make hex grid
 gridded <- st_make_grid(mke, square = FALSE, cellsize = 500)
 
+# limit hex grid to mke
 mke_grid <- gridded[mke] 
 
 mke_grid |> 
   ggplot() +
   geom_sf()
 
+# set up random index to populate grid
 ind <- sample(1:length(mke_grid), size = length(mke_grid), replace = FALSE)
 
-colors <- c("#F6A71C", "#00A6E1", "#0D213F")
-
-
+# not totally necessary here, but it's easier with dataframe
 df <- tibble(mke_grid) |> 
   rename(geometry = mke_grid) |> 
   st_as_sf()
 
-cs <- sample(colors, size = nrow(df), replace = TRUE)
-
-df <- bind_cols(df, col = cs)
-
+# Set up bounding box used to keep plot boundaries constant
 bb <- st_bbox(df)
 
+# do this to speed things up, populate multiple hexes at once
+# instead of one at a time
 ss <- seq(from = 1, to = length(ind), by = 3)
 
 # filling in background
 
 walk2(ss, 1:length(ss), function(x, y) {
   
+  # this is a buffer around the hex, used to simulate glowing
   big <- st_buffer(df[ind[c(x:(x+3))],], 200)
   
+  # map baby map
   df[ind[c(x:(x+3))],] |> 
     ggplot() +
+    # this geom is the background, all hexes mapped so far
     geom_sf(data = df[ind[1:x],], color = "#E4C601",
             alpha = .25, size = .1, fill = "#E4C601") +
+    # this geom is the glow for currently blinking
     geom_sf(data = big, size = .1, color = NA, fill = "white",
             alpha = .25) +
+    # this geom is the main for the currently blinking
     geom_sf(size = .1, color = "white", fill = "white") +
     annotate(geom = "text", label = "Milwaukee en Tour Eiffel",
              x = 1.08 * bb[["xmin"]],
@@ -56,7 +61,7 @@ walk2(ss, 1:length(ss), function(x, y) {
              family = "Aclonica",
              color = alpha("#E4C601", .75),
              size = 6) +
-    annotate(geom = "text", label = "Graphic by Spencer Schien (@MrPecners)",
+    annotate(geom = "text", label = "Réalisation: Spencer Schien (@MrPecners)",
              x = 1.025 * bb[["xmin"]],
              y = 1.005 * bb[["ymin"]],
              family = "Aclonica",
@@ -77,14 +82,19 @@ walk2(ss, 1:length(ss), function(x, y) {
 
 walk2(ss, length(ss) + 1:length(ss), function(x, y) {
   
+  # this is a buffer around the hex, used to simulate glowing
   big <- st_buffer(df[ind[c(x:(x+3))],], 200)
   
+  # map baby map
   df[ind[c(x:(x+3))],] |> 
     ggplot() +
+    # this geom is the background, all hexes mapped so far
     geom_sf(data = df, color = "#E4C601",
             alpha = .25, size = .1, fill = "#E4C601") +
+    # this geom is the glow for currently blinking
     geom_sf(data = big, size = .1, color = NA, fill = "white",
             alpha = .25) +
+    # this geom is the main for the currently blinking
     geom_sf(size = .1, color = "white", fill = "white") +
     annotate(geom = "text", label = "Milwaukee en Tour Eiffel",
              x = 1.08 * bb[["xmin"]],
@@ -92,7 +102,7 @@ walk2(ss, length(ss) + 1:length(ss), function(x, y) {
              family = "Aclonica",
              color = alpha("#E4C601", .75),
              size = 6) +
-    annotate(geom = "text", label = "Graphic by Spencer Schien (@MrPecners)",
+    annotate(geom = "text", label = "Réalisation: Spencer Schien (@MrPecners)",
              x = 1.025 * bb[["xmin"]],
              y = 1.005 * bb[["ymin"]],
              family = "Aclonica",
